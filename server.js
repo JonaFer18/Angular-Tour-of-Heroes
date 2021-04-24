add9464f1702f146f0413bd1e3f58ad884efb666
JonaFer18 <71918026+JonaFer18@users.noreply.github.com>
2021-04-24 01:01:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-'use strict';
-/*jshint esversion: 6 */
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080, function(){
-  console.log('Server started at http://localhost:8080/');
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/Rosales45'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/Rosales45/index.html'));
 });
-app.get('/', function(req, res) {
-  res.sendFile(require('path').join(__dirname + '/dist/index.html'))
-});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
