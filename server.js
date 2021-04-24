f07be25a1ceaf1a706cb28356409990445da205d
JonaFer18 <71918026+JonaFer18@users.noreply.github.com>
2021-04-24 00:16:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Angular-Tour-of-Heroes'));
+app.use(express.static('./dist/Angular-Tour-of-Heroes/src/'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Angular-Tour-of-Heroes'}),
+    res.sendFile('index.html', {root: 'dist/Angular-Tour-of-Heroes/src/'}),
 );
 
 // Start the app by listening on the default Heroku port
